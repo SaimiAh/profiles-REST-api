f28d72ce3dd2973f4fc362ee4de3c18abf9fc313
SaimiAh <ah.saim786@gmail.com>
2023-07-26 11:39:53 +0500

    Http Methods Test API

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from profiles_api import serializers
+from rest_framework import status
 
 class HelloApiView(APIView):
     """Making A Test Api View"""
+    serializer_class = serializers.HelloSerializer
     def get(self, request, format=None):
         """Return A APIView Features!"""
         api_view = [
@@ -13,3 +16,33 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message' : 'Hello!', 'api_view' : api_view})
+
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        """Handle updating an object"""
+
+        return Response({'method': 'PUT'})
+
+    def patch(self, request, pk=None):
+        """Handle partial update of object"""
+
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        """Delete an object"""
+
+        return Response({'method': 'DELETE'})
