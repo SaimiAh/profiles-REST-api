795b6270cbbb3c19cac38742211249101b8aea3a
SaimiAh <ah.saim786@gmail.com>
2023-07-27 16:25:59 +0500

    Made a Authorization Token and Login

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,10 +5,12 @@ from rest_framework.routers import DefaultRouter
 router=DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.ProfileFeedViewSet)
 
 
 
 urlpatterns = [
     path('hello-api/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
